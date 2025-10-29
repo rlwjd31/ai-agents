ded0fd976889174d02b69f1ca580776a5904462c
rlwjd31 <rlwjd31@naver.com>
2025-10-29 23:07:07 +0900

    [Feat]: Add search_tool with serper google

--- news-reader-crewai/tools.py
+++ news-reader-crewai/tools.py
@@ -1,13 +1,8 @@
-from crewai.tools import tool
+from crewai_tools import SerperDevTool
+from dotenv import load_dotenv
 
+load_dotenv()
 
-# docstring과 인자의 이름, type을 제대로 작성해야 CrewAI가 함수에 대한 schema를 정확히 생성할 수 있다.
-@tool
-def count_letters(sentence: str) -> int:
-    """
-    This function is to count the amount of letters in a sentence.
-    The input is a 'sentence' string.
-    The output is a number
-    """
+MAX_ARTICLES = 3
+news_search_tool = SerperDevTool(country="kr", n_results=MAX_ARTICLES)
 
-    return len(sentence)
