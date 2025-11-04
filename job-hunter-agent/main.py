24d06cc000c2d54c511a02fbb5e7ebbcb0ef4d1d
rlwjd31 <rlwjd31@naver.com>
2025-11-05 05:16:47 +0900

    [Feat]: Define agents & tasks of job hunter crew agent

---
+++ job-hunter-agent/main.py
@@ -0,0 +1,72 @@
+import dotenv
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, crew, agent, task
+
+
+@CrewBase
+class JobHunterCrew:
+    @agent
+    def job_search_agent(self) -> Agent:
+        return Agent(config=self.agents_config["job_search_agent"])
+
+    @agent
+    def job_matching_agent(self) -> Agent:
+        return Agent(config=self.agents_config["job_matching_agent"])
+
+    @agent
+    def resume_optimization_agent(self) -> Agent:
+        return Agent(config=self.agents_config["resume_optimization_agent"])
+
+    @agent
+    def company_research_agent(self) -> Agent:
+        return Agent(config=self.agents_config["company_research_agent"])
+
+    @agent
+    def interview_prep_agent(self) -> Agent:
+        return Agent(config=self.agents_config["interview_prep_agent"])
+
+    @task
+    def job_extraction_task(self) -> Task:
+        return Task(config=self.tasks_config["job_extraction_task"])
+
+    @task
+    def job_matching_task(self) -> Task:
+        return Task(config=self.tasks_config["job_matching_task"])
+
+    @task
+    def job_selection_task(self) -> Task:
+        return Task(config=self.tasks_config["job_selection_task"])
+
+    @task
+    def resume_rewriting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["resume_rewriting_task"],
+            # 이전 task가 job selection task라서 넣어주지 않아도 자동으로 들어가지만
+            # 명확하게 흐름을 알고 싶어 넣어줌.
+            context=[self.job_selection_task()],
+        )
+
+    @task
+    def company_research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["company_research_task"],
+            context=[self.job_selection_task()],
+        )
+
+    @task
+    def interview_prep_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["interview_prep_task"],
+            context=[
+                self.job_selection_task(),
+                self.resume_rewriting_task(),
+                self.company_research_task(),
+            ],
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        return Crew(agents=self.agents, tasks=self.tasks, verbose=True)
+
+
+JobHunterCrew().crew().kickoff()
